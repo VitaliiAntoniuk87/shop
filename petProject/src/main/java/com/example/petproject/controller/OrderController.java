0c10229523e681416281bfb59ce98ddf38e91742
AntoniukVitalii <antoniuk.vitalii@gmail.com>
2023-10-12 17:58:21 +0300

    init commit OrderController

--- petProject/src/main/java/com/example/petproject/controller/OrderController.java
+++ petProject/src/main/java/com/example/petproject/controller/OrderController.java
@@ -1,9 +1,20 @@
 package com.example.petproject.controller;
 
+import com.example.petproject.dto.OrderDTO;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/order")
 public class OrderController {
+
+    @GetMapping("/list/{userId}")
+    public List<OrderDTO> getOrderListByUserId(@PathVariable long userId) {
+
+    }
 }
